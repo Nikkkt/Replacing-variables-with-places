0f8ee2c71f8fe79b8923492435b377ffbe2a3c94
NTer <NTer@MACHINE-LGU4SHC>
2022-05-04 16:29:32 +0300

    Добавьте файлы проекта.

---
+++ Replacing variables with places.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    string a, b, c, d, e;
+
+    //with 3 variables
+    cout << "Enter the values of two variables: " << endl;
+
+    cout << "a = ";
+    cin >> a;
+    cout << "b = ";
+    cin >> b;
+
+    cout << "It was: a = " << a << ", b = " << b << endl;
+    c = a;
+    a = b;
+    b = c;
+
+    cout << "It became: a = " << a << ", b = " << b << endl;
+
+    //with 2 variabels
+    cout << "Enter the values of two variables: " << endl;
+
+    cout << "d = ";
+    cin >> d;
+    cout << "e = ";
+    cin >> e;
+
+    cout << "It was: d = " << d << ", e = " << e << endl;
+    swap(e, d);
+
+    cout << "It became: d = " << d << ", e = " << e << endl;
+}
